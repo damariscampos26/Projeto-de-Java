e627bf6d1963c5527f704a9e3c978fdc54d7a8b1
Dâmaris Campos <camposdamaris08@gmail.com>
2023-09-26 08:50:07 -0400

    Atualização do nome da variável

--- contabanco/src/ContaTerminal.java
+++ contabanco/src/ContaTerminal.java
@@ -5,19 +5,19 @@ public class ContaTerminal {
 
     public static void main(String[] args) {
 
-        Scanner variavel = new Scanner(System.in).useLocale(Locale.US);
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
 
         System.out.println("Por favor, digite o número da conta!");
-        int numero = variavel.nextInt();
+        int numero = scanner.nextInt();
 
         System.out.println("Por favor, digite o número da Agência!");
-        int agencia = variavel.nextInt();
+        int agencia = scanner.nextInt();
 
         System.out.println("Digite seu nome!");
-        String nomeCliente = variavel.next();
+        String nomeCliente = scanner.next();
 
         System.out.println("Digite seu saldo!");
-        double saldo = variavel.nextDouble();
+        double saldo = scanner.nextDouble();
 
         System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
         
